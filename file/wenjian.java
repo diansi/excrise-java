4e4e501ca7bf2246d15a7e45816c72320b175bfc
diansi <2397755156@qq.com>
2018-04-03 21:19:19 +0800

    读取文件内容输出到控制台

---
+++ file/wenjian.java
@@ -0,0 +1,22 @@
+import java.io.*;    
+public class wenjian {    
+    public static void main(String[] args) {    
+        File file = new File("E:\\信\\致你.txt");    
+        try {    
+            FileReader fr = new FileReader(file);    
+            BufferedReader reader = new BufferedReader(fr);    
+            String str = reader.readLine();    
+            while (str != null) {    
+                System.out.println(str);    
+                str = reader.readLine();    
+            }    
+        } 
+        catch (FileNotFoundException e) {   
+            //当抛出多个异常时，子异常当在父异常前抛出。  
+            e.printStackTrace();    
+        } 
+        catch (IOException e) {    
+            e.printStackTrace();    
+        }    
+    }      
+} 
